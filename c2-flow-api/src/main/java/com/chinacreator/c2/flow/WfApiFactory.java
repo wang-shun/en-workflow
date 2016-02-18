9b82f992f3178d3d6f5551d54dd8cfbfb1380c3c
hu.tan <hu.tan@chinacreator.com>
2016-02-18 14:59:47 +0800

    工作流服务端加入新版分布式配置方式

--- c2-flow-api/src/main/java/com/chinacreator/c2/flow/WfApiFactory.java
+++ c2-flow-api/src/main/java/com/chinacreator/c2/flow/WfApiFactory.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.context.ContextLoader;
 
@@ -15,7 +14,7 @@ import com.chinacreator.c2.flow.api.WfRepositoryService;
 import com.chinacreator.c2.flow.api.WfRuntimeService;
 
 public class WfApiFactory {
-	static ApplicationContext factoryRemote = null;
+	
 	static ApplicationContext factoryLocal = null;
 
 	private static WfRuntimeService wfRuntimeService;
@@ -34,21 +33,18 @@ public class WfApiFactory {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 		remoted = (String)p.get("c2.flow.api.remoted");
 		holidayWorkDayFlag = (String)p.get("c2.flow.api.holidayworkday.flag");
 //		remoted = (String) WfPropertyPlaceholderConfigurer
 //				.getContextProperty("c2.flow.api.remoted");
-		if ("true".equals(remoted)) {
-			factoryRemote = new ClassPathXmlApplicationContext(
-					"classpath:custom/c2-flow-api-application.xml");
-		}else{
-			factoryLocal = ContextLoader.getCurrentWebApplicationContext();
-		}
+
+		factoryLocal = ContextLoader.getCurrentWebApplicationContext();
 	}
 
 	public static WfRuntimeService getWfRuntimeService() {
 		if ("true".equals(remoted)) {
-			wfRuntimeService = (WfRuntimeService) factoryRemote
+			wfRuntimeService = (WfRuntimeService) factoryLocal
 					.getBean("wfRuntimeServiceRemote");
 		} else {
 			wfRuntimeService = (WfRuntimeService) factoryLocal.getBean("wfRuntimeServiceLocal");
@@ -58,7 +54,7 @@ public class WfApiFactory {
 
 	public static WfRepositoryService getWfRepositoryService() {
 		if ("true".equals(remoted)) {
-			wfRepositoryService = (WfRepositoryService) factoryRemote
+			wfRepositoryService = (WfRepositoryService) factoryLocal
 					.getBean("wfRepositoryServiceRemote");
 		} else {
 			wfRepositoryService = (WfRepositoryService) factoryLocal.getBean("wfRepositoryServiceLocal");
@@ -68,7 +64,7 @@ public class WfApiFactory {
 
 	public static WfHistoryService getWfHistoryService() {
 		if ("true".equals(remoted)) {
-			wfHistoryService = (WfHistoryService) factoryRemote
+			wfHistoryService = (WfHistoryService) factoryLocal
 					.getBean("wfHistoryServiceRemote");
 		} else {
 			wfHistoryService = (WfHistoryService) factoryLocal.getBean("wfHistoryServiceLocal");
@@ -78,7 +74,7 @@ public class WfApiFactory {
 
 	public static WfManagerService getWfManagerService() {
 		if ("true".equals(remoted)) {
-			wfManagerService = (WfManagerService) factoryRemote
+			wfManagerService = (WfManagerService) factoryLocal
 					.getBean("wfManagerServiceRemote");
 		} else {
 			wfManagerService = (WfManagerService) factoryLocal.getBean("wfManagerServiceLocal");
@@ -88,7 +84,7 @@ public class WfApiFactory {
 
 	public static WfFormService getWfFormService() {
 		if ("true".equals(remoted)) {
-			wfFormService = (WfFormService) factoryRemote
+			wfFormService = (WfFormService) factoryLocal
 					.getBean("wfFormServiceRemote");
 		} else {
 			wfFormService = (WfFormService) factoryLocal.getBean("wfFormServiceLocal");
